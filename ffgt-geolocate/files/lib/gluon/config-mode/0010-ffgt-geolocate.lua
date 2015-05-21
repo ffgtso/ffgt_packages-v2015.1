f096ef8e6509f8918ce2b3b5ab99f30c367d573b
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-21 03:29:12 +0200

    Run from within config-mode ...

---
+++ ffgt-geolocate/files/lib/gluon/config-mode/0010-ffgt-geolocate.lua
@@ -0,0 +1,9 @@
+local cbi = require "luci.cbi"
+local uci = luci.model.uci.cursor()
+
+local lat = uci:get("gluon-node-info", 'location', "latitude")
+local lon = uci:get("gluon-node-info", 'location', "longitude")
+if not lat or not lon then
+    os.execute('sh "/lib/gluon/site-upgrade"')
+    os.execute('sleep 20')
+end
