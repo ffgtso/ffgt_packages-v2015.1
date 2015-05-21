54973967f411238269e637efa8295a78f0b52d5c
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-22 01:56:35 +0200

    Removed presets.

--- gluon-config-mode-mesh-vpn-ffgt/files/lib/gluon/config-mode/wizard/0300-mesh-vpn.lua
+++ gluon-config-mode-mesh-vpn-ffgt/files/lib/gluon/config-mode/wizard/0300-mesh-vpn.lua
@@ -4,8 +4,6 @@ local uci = luci.model.uci.cursor()
 local M = {}
 
 function M.section(form)
-  setup_fastd_secret("mesh-vpn")
-
   local s = form:section(cbi.SimpleSection, nil,
     [[Bitte nenne die Bandbreite, die Du f&uuml;r Deinen Freifunk-Knoten
     an Deinem Internetzugang zum Teilen freigeben willst. Der Wert 0 schaltet
@@ -59,7 +57,6 @@ function M.handle(data)
 
     if data._limit_ingress ~= nil and data._limit_egress ~= nil and data._limit_egress + 0 > 0 and data._limit_ingress + 0 > 0 then
       uci:set("gluon-simple-tc", meshvpn_name, "enabled", "1")
-      uci:set("gluon-simple-tc", meshvpn_name, "enabled", "1")
     end
 
     uci:commit("gluon-simple-tc")
@@ -68,38 +65,6 @@ function M.handle(data)
     uci:save("fastd")
     uci:commit("fastd")
 --  end
-end
-
-function setup_fastd_secret(name)
-  local uci = luci.model.uci.cursor()
-  local secret = uci:get("fastd", "mesh_vpn", "secret")
-
-  if not secret or not secret:match("%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x") then
-    local f = io.popen("fastd --generate-key --machine-readable", "r")
-    local secret = f:read("*a")
-    f:close()
-
-    uci:set("fastd", "mesh_vpn", "secret", secret)
-    uci:save("fastd")
-    uci:commit("fastd")
-
-    uci:set("autoupdater", "settings", "enabled", "1")
-    uci:save("autoupdater")
-    uci:commit("autoupdater")
-
-    uci:set("fastd", "mesh_vpn", "enabled", "1")
-    uci:save("fastd")
-    uci:commit("fastd")
-
-    uci:set("gluon-simple-tc", "mesh_vpn", "interface")
-    uci:set("gluon-simple-tc", "mesh_vpn", "ifname", "mesh-vpn")
-    uci:set("gluon-simple-tc", "mesh_vpn", "enabled", "0")
-
-    local sname = uci:get_first("gluon-node-info", "location")
-    uci:set("gluon-node-info", sname, "share_location", "1")
-    uci:save("gluon-node-info")
-    uci:commit("gluon-node-info")
-  end
 end
 
 return M
