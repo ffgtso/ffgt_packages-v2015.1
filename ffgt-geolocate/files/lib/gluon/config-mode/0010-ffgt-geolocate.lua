1d8695cc22c84f4d72a8e12b0937b306d50ef996
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-21 03:31:24 +0200

    ... now run the proper file ;)

--- ffgt-geolocate/files/lib/gluon/config-mode/0010-ffgt-geolocate.lua
+++ ffgt-geolocate/files/lib/gluon/config-mode/0010-ffgt-geolocate.lua
@@ -4,6 +4,6 @@ local uci = luci.model.uci.cursor()
 local lat = uci:get("gluon-node-info", 'location', "latitude")
 local lon = uci:get("gluon-node-info", 'location', "longitude")
 if not lat or not lon then
-    os.execute('sh "/lib/gluon/site-upgrade"')
+    os.execute('sh "/lib/gluon/ffgt-geolocate/senddata.sh"')
     os.execute('sleep 20')
 end
