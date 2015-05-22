9a68a6ce6eea01d7eaa3b7366c560a9f50066d0c
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-22 17:08:40 +0200

    Another language, another set of issues :(

--- gluon-config-mode-site-select-ffgt/files/lib/gluon/config-mode/wizard/0200-site-select.lua
+++ gluon-config-mode-site-select-ffgt/files/lib/gluon/config-mode/wizard/0200-site-select.lua
@@ -34,7 +34,7 @@ function M.section(form)
 
         local unlocode = uci:get_first("gluon-node-info", "location", "locode")
 	    if uci:get_first("gluon-setup-mode", "setup_mode", "configured") == "0" then
-	    	o:value(unlocode, uci:get('siteselect', unlocode, 'sitename'))
+	    	o:value(unlocode, uci:get_first('siteselect', unlocode, 'sitename'))
 	    else
 		    o:value(site.site_code, site.site_name)
 	    end
@@ -46,44 +46,78 @@ function M.section(form)
         local s = form:section(cbi.SimpleSection, nil, [[Geo-Lokalisierung erfolgreich.]])
         local unlocode = uci:get_first("gluon-node-info", "location", "locode")
         local o = s:option(cbi.DummyValue, "community", "Community")
+    	o.rmempty = false
+        o.optional = false
         o.value = unlocode
+        -- FIXME! Why isn't this working below? It works with cbi.Value or cbi.ListValue,
+        -- but with cbi.DummyValue I get:
+        -- /lib/gluon/config-mode/wizard//0200-site-select.lua:66: bad argument #2 to 'get' (string expected, got nil)
+        -- (with line 66: local secret = uci:get_first('siteselect', data.community, 'secret'))
     end
 end
 
 function M.handle(data)
+    if data.community then
+        if data.community ~= site.site_code then
+            uci:set('siteselect', site.site_code, "secret", uci:get('fastd', 'mesh_vpn', 'secret'))
+            uci:save('siteselect')
+            uci:commit('siteselect')
 
-	if data.community ~= site.site_code then
-		uci:set('siteselect', site.site_code, "secret", uci:get('fastd', 'mesh_vpn', 'secret'))
-		uci:save('siteselect')
-		uci:commit('siteselect')
+            -- Deleting this unconditionally would leave the node without a secret in case the
+            -- check fails later on. Moving the delete down into the if-clauses.
+            -- -- uci:delete('fastd', 'mesh_vpn', 'secret')
 
-        -- Deleting this unconditionally would leave the node without a secret in case the
-        -- check fails later on. Moving the delete down into the if-clauses.
-		-- uci:delete('fastd', 'mesh_vpn', 'secret')
+            local secret = uci:get_first('siteselect', data.community, 'secret')
 
-		local secret = uci:get('siteselect', data.community, 'secret')
-		
-		if not secret or not secret:match(("%x"):rep(64)) then
-			uci:delete('siteselect', data.community, 'secret')
-		else
-			uci:delete('fastd', 'mesh_vpn', 'secret')
+            if not secret or not secret:match(("%x"):rep(64)) then
+                uci:delete('siteselect', data.community, 'secret')
+            else
+                uci:delete('fastd', 'mesh_vpn', 'secret')
+                uci:set('fastd', 'mesh_vpn', "secret", secret)
+            end
+
+            uci:save('fastd')
+            uci:commit('fastd')
+
+            -- We need to store the selection somewhere. To make this simple,
+            -- put it into gluon-node-info:location.siteselect ...
+            uci:delete('gluon-node-info', 'location', 'siteselect')
+            uci:set('gluon-node-info', 'location', 'siteselect', data.community)
+            uci:save('gluon-node-info')
+            uci:commit('gluon-node-info')
+
+            fs.copy(uci:get('siteselect', data.community , 'path'), '/lib/gluon/site.conf')
+            os.execute('sh "/lib/gluon/site-upgrade"')
+        end
+    else
+        -- The UN/LOCODE is the relevant information. No user servicable parts in the UI ;)
+        local unlocode = uci:get_first("gluon-node-info", "location", "locode")
+        local current = uci:get_first('gluon-node-info', 'location', 'siteselect')
+
+        local secret = uci:get_first('siteselect', uncode, 'secret')
+
+        if not secret or not secret:match(("%x"):rep(64)) then
+            uci:delete('siteselect', uncode, 'secret')
+            uci:save('siteselect')
+            uci:commit('siteselect')
+        else
+            uci:delete('fastd', 'mesh_vpn', 'secret')
             uci:set('fastd', 'mesh_vpn', "secret", secret)
-		end
-				
-		uci:save('fastd')
-		uci:commit('fastd')
+            uci:save('fastd')
+            uci:commit('fastd')
+        end
+
 
         -- We need to store the selection somewhere. To make this simple,
         -- put it into gluon-node-info:location.siteselect ...
         uci:delete('gluon-node-info', 'location', 'siteselect')
-        uci:set('gluon-node-info', 'location', 'siteselect', data.community)
+        uci:set('gluon-node-info', 'location', 'siteselect', unlocode)
         uci:save('gluon-node-info')
         uci:commit('gluon-node-info')
 
-		fs.copy(uci:get('siteselect', data.community , 'path'), '/lib/gluon/site.conf')
-		
-		os.execute('sh "/lib/gluon/site-upgrade"')
-	end
+        fs.copy(uci:get('siteselect', unlocode, 'path'), '/lib/gluon/site.conf')
+        os.execute('sh "/lib/gluon/site-upgrade"')
+    end
 end
 
 return M
