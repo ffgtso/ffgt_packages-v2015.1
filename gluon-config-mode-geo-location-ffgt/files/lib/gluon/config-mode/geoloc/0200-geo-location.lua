a45475bd03465ddd796a22b4dee61a7aebf93fb8
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-23 04:36:26 +0200

    Next try ...

--- gluon-config-mode-geo-location-ffgt/files/lib/gluon/config-mode/geoloc/0200-geo-location.lua
+++ gluon-config-mode-geo-location-ffgt/files/lib/gluon/config-mode/geoloc/0200-geo-location.lua
@@ -10,36 +10,38 @@ function M.section(form)
     hinterlegen.]])
 
   local o
+  local lat = uci:get_first("gluon-node-info", 'location', "latitude")
+  local lon = uci:get_first("gluon-node-info", 'location', "longitude")
+  if not lat then lat=0 end
+  if not lon then lon=0 end
+  if ((lat == 0) or (lat == 51)) and ((lon == 0) or (lon == 9)) then
+    o = s:option(cbi.Value, "_zip", "Postleitzahl")
+    o.default = "33333"
+    o.rmempty = false
 
-  o = s:option(cbi.Flag, "_location", "Knoten auf der Karte anzeigen")
-  o.default = uci:get_first("gluon-node-info", "location", "share_location", o.disabled)
-  o.rmempty = false
+    lat = "51.908624626589585"
+    lon = "8.380953669548035"
+  end
 
   o = s:option(cbi.Value, "_latitude", "Breitengrad")
   o.default = uci:get_first("gluon-node-info", "location", "latitude")
-  o:depends("_location", "1")
   o.rmempty = false
   o.datatype = "float"
   o.description = "z.B. 53.873621"
 
   o = s:option(cbi.Value, "_longitude", "Längengrad")
   o.default = uci:get_first("gluon-node-info", "location", "longitude")
-  o:depends("_location", "1")
   o.rmempty = false
   o.datatype = "float"
   o.description = "z.B. 10.689901"
 
-  local mylat = uci:get_first("gluon-node-info", "location", "latitude")
-  local mylon = uci:get_first("gluon-node-info", "location", "longitude")
-  local mystr = string.format("Hier sollte unsere Karte zu sehen sein, sofern Dein Computer Internet-Zugang hat. Einfach die Karte auf Deinen Standort ziehen, den Button zur Koordinatenanzeige klicken und dann die Daten in die Felder oben kopieren:<p><iframe src=\"http://stats.guetersloh.freifunk.net/map/geomap.html?lat=%f&amp;lon=%f\" width=\"100%%\" height=\"700\">Unsere Knotenkarte</iframe></p>", mylat, mylon)
-
+  local mystr = string.format("Hier sollte unsere Karte zu sehen sein, sofern Dein Computer Internet-Zugang hat. Einfach die Karte auf Deinen Standort ziehen, den Button zur Koordinatenanzeige klicken und dann die Daten in die Felder oben kopieren:<p><iframe src=\"http://stats.guetersloh.freifunk.net/map/geomap.html?lat=%f&amp;lon=%f\" width=\"100%%\" height=\"700\">Unsere Knotenkarte</iframe></p>", lat, lon)
   local s = form:section(cbi.SimpleSection, nil, mystr)
 end
 
 function M.handle(data)
   local sname = uci:get_first("gluon-node-info", "location")
 
-  uci:set("gluon-node-info", sname, "share_location", data._location)
   if data._location and data._latitude ~= nil and data._longitude ~= nil then
     uci:set("gluon-node-info", sname, "latitude", data._latitude)
     uci:set("gluon-node-info", sname, "longitude", data._longitude)
