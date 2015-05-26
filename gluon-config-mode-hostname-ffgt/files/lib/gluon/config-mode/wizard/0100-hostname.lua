b1d02fc6fd2cb6b71c43b57c4b88ee53bea02949
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-27 00:04:29 +0200

    Print suggested name.

--- gluon-config-mode-hostname-ffgt/files/lib/gluon/config-mode/wizard/0100-hostname.lua
+++ gluon-config-mode-hostname-ffgt/files/lib/gluon/config-mode/wizard/0100-hostname.lua
@@ -1,14 +1,25 @@
 local cbi = require "luci.cbi"
 local uci = luci.model.uci.cursor()
+local util = require 'gluon.util'
 
 local M = {}
 
 function M.section(form)
+  local hostname = uci:get_first("system", "system", "hostname")
   local s = form:section(cbi.SimpleSection, nil, nil)
   local o = s:option(cbi.Value, "_hostname", "Name dieses Knotens")
-  o.value = uci:get_first("system", "system", "hostname")
+  o.value = hostname
   o.rmempty = false
   o.datatype = "hostname"
+
+  local addr = uci:get_first("gluon-node-info", 'location', "addr")
+  local zip = uci:get_first("gluon-node-info", 'location', "zip")
+  local mac = string.sub(util.node_id(), 9)
+  local mystr = string.format("%s-%s-%s", zip, addr, mac)
+  if mystr ~= hostname then
+    local o = s:option(cbi.DummyValue, "_defaulthostname", "Namensvorschlag")
+    o.value = mystr
+  end
 end
 
 function M.handle(data)
