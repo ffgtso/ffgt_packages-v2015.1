ff2dcd3c5780f74f9f830202abc7331bb3748e44
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-26 19:25:52 +0200

    Bugfixes

--- ffgt-geolocate/files/lib/gluon/ffgt-geolocate/senddata.sh
+++ ffgt-geolocate/files/lib/gluon/ffgt-geolocate/senddata.sh
@@ -16,6 +16,13 @@ if [ "$isconfigured" != "1" ]; then
  isconfigured=0
 fi
 
+if [ $# -eq 1 ]; then
+  forcerun=1
+  runnow=1
+else
+  forcerun=0
+fi
+
 if [ -e /tmp/run/wifi-data-sent ]; then
  runnow=0
 fi
@@ -61,7 +68,8 @@ fi
  if [ -e /tmp/run/wifi-data-sent ]; then
   curlat="`/sbin/uci get gluon-node-info.@location[0].longitude 2>/dev/null`"
   mobile="`/sbin/uci get gluon-node-info.@location[0].is_mobile 2>/dev/null`"
-  if [ "X${curlat}" = "X" -o "X${mobile}" = "X1" ]; then
+  if [ "X${curlat}" = "X" -o "X${mobile}" = "X1" -o ${forcerun} -eq 1 ]; then
+   /bin/cat /dev/null >/tmp/geoloc.sh
    sleep 2
    /usr/bin/wget -q -O /tmp/geoloc.out "http://setup.${IPVXPREFIX}guetersloh.freifunk.net/geoloc.php?list=me&node=$mac"
    if [ -e /tmp/geoloc.out ]; then
@@ -71,15 +79,14 @@ fi
      echo "/sbin/uci add gluon-node-info location" >>/tmp/geoloc.sh
     fi
     # Honour existing share_location setting; if missing, create & set to '1'
-    hasshare="`/sbin/uci get gluon-node-info.@location[0].share_location 1>/dev/null 2>&1; echo $?`"
+    hasshare="`/sbin/uci get gluon-node-info.@location[0].share_location >/dev/null 2>&1; echo $?`"
     if [ "${hasshare}" != "0" ]; then
      echo "/sbin/uci set gluon-node-info.@location[0].share_location=1" >>/tmp/geoloc.sh
     fi
     grep "LAT: 0" </tmp/geoloc.out >/dev/null 2>&1
     if [ $? -ne 0 ]; then
-     /bin/cat /dev/null >/tmp/geoloc.sh
      /usr/bin/awk </tmp/geoloc.out '/^LAT:/ {printf("/sbin/uci set gluon-node-info.@location[0].latitude=%s\n", $2);} /^LON:/ {printf("/sbin/uci set gluon-node-info.@location[0].longitude=%s\n", $2);}' >>/tmp/geoloc.sh
-     /usr/bin/awk </tmp/geoloc.out '/^ADR:/ {printf("/sbin/uci set gluon-node-info.@location[0].addr=%c%s%c\n", 39, substr($0, 6), 39);} /^CTY:/ {printf("/sbin/uci set gluon-node-info.@location[0].city=%c%s%c\n", substr($0, 6));}' >>/tmp/geoloc.sh
+     /usr/bin/awk </tmp/geoloc.out '/^ADR:/ {printf("/sbin/uci set gluon-node-info.@location[0].addr=%c%s%c\n", 39, substr($0, 6), 39);} /^CTY:/ {printf("/sbin/uci set gluon-node-info.@location[0].city=%c%s%c\n", 39, substr($0, 6), 39);}' >>/tmp/geoloc.sh
      /usr/bin/awk </tmp/geoloc.out '/^LOC:/ {printf("/sbin/uci set gluon-node-info.@location[0].locode=%s\n", $2)}; /^ZIP:/ {printf("/sbin/uci set gluon-node-info.@location[0].zip=%s\n", $2);} END{printf("/sbin/uci commit gluon-node-info\n");}' >>/tmp/geoloc.sh
      /bin/sh /tmp/geoloc.sh
      if [ $isconfigured -ne 1 ]; then
