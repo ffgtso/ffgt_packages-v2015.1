c0628c7a331f1d70cf2298ae48b612e257edc173
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-28 17:51:30 +0200

    Fix indention, show community name

--- ffgt-geolocate/files/lib/gluon/config-mode/geoloc/0010-ffgt-geolocate.lua
+++ ffgt-geolocate/files/lib/gluon/config-mode/geoloc/0010-ffgt-geolocate.lua
@@ -1,34 +1,38 @@
 local cbi = require "luci.cbi"
 local uci = luci.model.uci.cursor()
+local sys = luci.sys
 
 local M = {}
 
 function M.section(form)
-    local lat = uci:get_first("gluon-node-info", 'location', "latitude")
-    local lon = uci:get_first("gluon-node-info", 'location', "longitude")
-    if not lat then lat=0 end
-    if not lon then lon=0 end
-    if (lat == 0) and (lon == 0) then
-        local s = form:section(cbi.SimpleSection, nil,
-        [[<b>Es sind keine Koordinaten hinterlegt.</b> Bitte trage sie ein oder versuche die
-        automatische Lokalisierung (anhand der empfangenen Funknetze bzw. der IP-Adresse)
-        &uuml;ber die Schaltfl&auml;che "Geolocate" oben. Bitte beachte, da&szlig; Dein
-        Knoten über die gelben(!) Ports Internet-Zugang haben mu&szlig;.]])
-    elseif (lat == "51") and (lon == "9") then
-        local s = form:section(cbi.SimpleSection, nil,
-        [[<b>Die automatische Lokalisierung ist fehlgeschlagen.</b> Bitte trage Deine
-        Koordinaten, gerne mit Hilfe der Karte, ein. Bitte beachte, da&szlig; Dein
-        Knoten über die gelben(!) Ports Internet-Zugange haben mu&szlig;,
-        damit die Karte angezeigt und die Daten validiert werden k&ouml;nnen.]])
-    else
-        local addr = uci:get_first("gluon-node-info", 'location', "addr")
-        local city = uci:get_first("gluon-node-info", 'location', "city")
-        local zip = uci:get_first("gluon-node-info", 'location', "zip")
-        local unlocode = uci:get_first("gluon-node-info", "location", "locode")
-        local community = uci:get_first('siteselect', unlocode, 'sitename') or unlocode
-        local mystr = string.format("Lokalisierung des Knotens erfolgreich; bitte Daten &uuml;berpr&uuml;fen:<br></br><b>Adresse:</b> %s, %s %s<br></br><b>Koordinaten:</b> %f %f<br></br><b>Community:</b> %s", addr, zip, city, lat, lon, community)
-        local s = form:section(cbi.SimpleSection, nil, mystr)
+  local lat = uci:get_first("gluon-node-info", 'location', "latitude")
+  local lon = uci:get_first("gluon-node-info", 'location', "longitude")
+  if not lat then lat=0 end
+  if not lon then lon=0 end
+  if (lat == 0) and (lon == 0) then
+    local s = form:section(cbi.SimpleSection, nil,
+    [[<b>Es sind keine Koordinaten hinterlegt.</b> Bitte trage sie ein oder versuche die
+    automatische Lokalisierung (anhand der empfangenen Funknetze bzw. der IP-Adresse)
+    &uuml;ber die Schaltfl&auml;che "Geolocate" oben. Bitte beachte, da&szlig; Dein
+    Knoten über die gelben(!) Ports Internet-Zugang haben mu&szlig;.]])
+  elseif (lat == "51") and (lon == "9") then
+    local s = form:section(cbi.SimpleSection, nil,
+    [[<b>Die automatische Lokalisierung ist fehlgeschlagen.</b> Bitte trage Deine
+    Koordinaten, gerne mit Hilfe der Karte, ein. Bitte beachte, da&szlig; Dein
+    Knoten über die gelben(!) Ports Internet-Zugange haben mu&szlig;,
+    damit die Karte angezeigt und die Daten validiert werden k&ouml;nnen.]])
+  else
+    local addr = uci:get_first("gluon-node-info", 'location', "addr")
+    local city = uci:get_first("gluon-node-info", 'location', "city")
+    local zip = uci:get_first("gluon-node-info", 'location', "zip")
+    local unlocode = uci:get_first("gluon-node-info", "location", "locode")
+    local community = uci:get_first('siteselect', unlocode, 'sitename') or unlocode
+    if community == unlocode then
+      community=string.gsub(sys.exec(string.format('/sbin/uci get siteselect.%s.sitename', unlocode)), "\n", "")
     end
+    local mystr = string.format("Lokalisierung des Knotens erfolgreich; bitte Daten &uuml;berpr&uuml;fen:<br></br><b>Adresse:</b> %s, %s %s<br></br><b>Koordinaten:</b> %f %f<br></br><b>Community:</b> %s", addr, zip, city, lat, lon, community)
+    local s = form:section(cbi.SimpleSection, nil, mystr)
+  end
 end
 
 function M.handle(data)
