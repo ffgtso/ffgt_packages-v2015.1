af48607d94648b7111c216a9ad3bd7ffc9ac0ee3
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-30 16:29:33 +0200

    Refactored; new default zip-freifunk-nodeid.

--- ffgt-geolocate/files/lib/gluon/ffgt-geolocate/senddata.sh
+++ ffgt-geolocate/files/lib/gluon/ffgt-geolocate/senddata.sh
@@ -96,8 +96,10 @@ if [ ${runnow} -eq 1 ]; then
       adr="`/sbin/uci get gluon-node-info.@location[0].addr 2>/dev/null`"
       zip="`/sbin/uci get gluon-node-info.@location[0].zip 2>/dev/null`"
       if [ "x${zip}" != "x" -a "x${adr}" != "x" ]; then
-       suffix=`echo "util=require 'gluon.util' print(string.format('%s', util.node_id()))" | /usr/bin/lua | awk '{print substr($1, 9);}'`
-       hostname="${zip}-${adr}-${suffix}"
+       nodeid=`echo "util=require 'gluon.util' print(string.format('%s', string.sub(util.node_id(), 9)))" | /usr/bin/lua`
+       #suffix=`echo "util=require 'gluon.util' print(string.format('%s', string.sub(util.node_id(), 9)))" | /usr/bin/lua`
+       #hostname="${zip}-${adr}-${suffix}"
+       hostname="${zip}-freifunk-${nodeid}"
        /sbin/uci set system.@system[0].hostname="${hostname}"
        /sbin/uci commit system
       fi
