32e078f690fe22ec847820e15d02f6c8a03ccce3
Kai 'wusel' Siering <wusel+src@uu.org>
2015-06-01 21:04:12 +0200

    If lot/lon == 0, point to overview of joined map, otherwise zoom to location.

--- gluon-config-mode-geo-location-ffgt/files/lib/gluon/config-mode/geoloc/0200-geo-location.lua
+++ gluon-config-mode-geo-location-ffgt/files/lib/gluon/config-mode/geoloc/0200-geo-location.lua
@@ -49,7 +49,11 @@ function M.section(form)
   o.description = "z.B. 10.689901"
   o.optional = false
 
-  local mystr = string.format("Hier sollte unsere Karte zu sehen sein, sofern Dein Computer Internet-Zugang hat. Einfach die Karte auf Deinen Standort ziehen, den Button zur Koordinatenanzeige klicken und dann die Daten in die Felder oben kopieren:<p><iframe src=\"http://stats.guetersloh.freifunk.net/map/geomap.html?lat=%f&amp;lon=%f\" width=\"100%%\" height=\"700\">Unsere Knotenkarte</iframe></p>", maplat, maplon)
+  if lat == 0 and lon == 0 then
+    local mystr = string.format("Hier sollte unsere &Uuml;bersichtskarte zu sehen sein, sofern Dein Computer Internet-Zugang hat. Einfach die Karte auf Deinen Standort ziehen, den Button zur Koordinatenanzeige klicken und dann die Daten in die Felder oben kopieren:<p><iframe src=\"http://map.4830.org/geomap.html" width=\"100%%\" height=\"700\">Unsere Knotenkarte</iframe></p>")
+  else
+    local mystr = string.format("Hier sollte unsere Karte zu sehen sein, sofern Dein Computer Internet-Zugang hat. Einfach die Karte auf Deinen Standort ziehen, den Button zur Koordinatenanzeige klicken und dann die Daten in die Felder oben kopieren:<p><iframe src=\"http://map.4830.org/geomap.html?lat=%f&amp;lon=%f\" width=\"100%%\" height=\"700\">Unsere Knotenkarte</iframe></p>", maplat, maplon)
+  end
   local s = form:section(cbi.SimpleSection, nil, mystr)
 end
 
