702b450160345d089426d4ecd52732b2b2b659d5
Kai 'wusel' Siering <wusel+src@uu.org>
2015-06-08 18:14:26 +0200

    Check for locode.

--- ffgt-geolocate/files/lib/gluon/config-mode/geoloc/0010-ffgt-geolocate.lua
+++ ffgt-geolocate/files/lib/gluon/config-mode/geoloc/0010-ffgt-geolocate.lua
@@ -7,6 +7,7 @@ local M = {}
 function M.section(form)
   local lat = uci:get_first("gluon-node-info", 'location', "latitude")
   local lon = uci:get_first("gluon-node-info", 'location', "longitude")
+  local unlocode = uci:get_first("gluon-node-info", "location", "locode")
   if not lat then lat=0 end
   if not lon then lon=0 end
   if (lat == 0) and (lon == 0) then
@@ -19,13 +20,18 @@ function M.section(form)
     local s = form:section(cbi.SimpleSection, nil,
     [[<b>Die automatische Lokalisierung ist fehlgeschlagen.</b> Bitte trage Deine
     Koordinaten, gerne mit Hilfe der Karte, ein. Bitte beachte, da&szlig; Dein
-    Knoten über die gelben(!) Ports Internet-Zugange haben mu&szlig;,
+    Knoten über die gelben(!) Ports Internet-Zugang haben mu&szlig;,
+    damit die Karte angezeigt und die Daten validiert werden k&ouml;nnen.]])
+  elseif not unlocode then
+    local s = form:section(cbi.SimpleSection, nil,
+    [[<b>Die Adressaufl&ouml;sung ist fehlgeschlagen.</b> Bitte &uuml;berpr&uuml;fe Deine
+    Koordinaten, sie konnten keinem Ort zugeordnet werden. Bitte beachte, da&szlig; Dein
+    Knoten über die gelben(!) Ports Internet-Zugang haben mu&szlig;,
     damit die Karte angezeigt und die Daten validiert werden k&ouml;nnen.]])
   else
     local addr = uci:get_first("gluon-node-info", 'location', "addr") or "FEHLER_ADDR"
     local city = uci:get_first("gluon-node-info", 'location', "city") or "FEHLER_ORT"
     local zip = uci:get_first("gluon-node-info", 'location', "zip") or "00000"
-    local unlocode = uci:get_first("gluon-node-info", "location", "locode")
     local community = uci:get_first('siteselect', unlocode, 'sitename') or unlocode
     if community == unlocode then
       community=string.gsub(sys.exec(string.format('/sbin/uci get siteselect.%s.sitename', unlocode)), "\n", "")
