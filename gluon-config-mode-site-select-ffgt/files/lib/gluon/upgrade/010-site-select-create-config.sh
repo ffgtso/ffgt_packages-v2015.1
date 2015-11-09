4f4a28f044eb7c0e4144f23d0b623444b933757c
Kai 'wusel' Siering <wusel+src@uu.org>
2015-11-09 19:02:23 +0100

    Moved extra to files/lib/gluon/site-select; no more copying at image creation; site-select-UCI config will be (re-) created during each run of upgrade scripts from /lib/gluon/site-select

--- gluon-config-mode-site-select-ffgt/files/lib/gluon/upgrade/010-site-select-create-config.sh
+++ gluon-config-mode-site-select-ffgt/files/lib/gluon/upgrade/010-site-select-create-config.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-echo "" > "$DIR"/etc/config/siteselect
+echo "" > /etc/config/siteselect
 
-/bin/ls -1 "$GLUON_SITEDIR"/extra/ | while read FILE
+/bin/ls -1 /lib/gluon/site-select/ | while read FILE
 do
-site_name="$(cat "$GLUON_SITEDIR"/extra/"$FILE" | grep "site_name" | sed "s/site_name =//; s/,//")"
-#site_code="$(cat "$GLUON_SITEDIR"/extra/"$FILE" | grep "site_code" | sed "s/site_code =//; s/,//")"
+site_name="$(cat lib/gluon/site-select/"$FILE" | grep "site_name" | sed "s/site_name =//; s/,//")"
+#site_code="$(cat "$GLUON_SITEDIR"/site-select/"$FILE" | grep "site_code" | sed "s/site_code =//; s/,//")"
 # We use an external code (the UN/LOCODE (http://www.unece.org/cefact/locode/welcome.html) to select
 # which site.conf should be used where ...
 site_code=" $(basename "$FILE" .conf)"
 
-echo "config site"$site_code"" >> "$DIR"/etc/config/siteselect
-echo "    option path '/lib/gluon/site-select/"$FILE"'" >> "$DIR"/etc/config/siteselect
-echo "    option sitename"$site_name"" >> "$DIR"/etc/config/siteselect
-echo "" >> "$DIR"/etc/config/siteselect
+echo "config site"$site_code"" >> /etc/config/siteselect
+echo "    option path '/lib/gluon/site-select/"$FILE"'" >> /etc/config/siteselect
+echo "    option sitename"$site_name"" >> /etc/config/siteselect
+echo "" >> /etc/config/siteselect
 
 done
