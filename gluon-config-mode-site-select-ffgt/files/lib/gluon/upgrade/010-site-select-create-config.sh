9be2afee63c60f3eb3343ee72dba13e9eae79555
Kai 'wusel' Siering <wusel+src@uu.org>
2015-11-10 01:14:12 +0100

    Fix missing /

--- gluon-config-mode-site-select-ffgt/files/lib/gluon/upgrade/010-site-select-create-config.sh
+++ gluon-config-mode-site-select-ffgt/files/lib/gluon/upgrade/010-site-select-create-config.sh
@@ -4,7 +4,7 @@ echo "" > /etc/config/siteselect
 
 /bin/ls -1 /lib/gluon/site-select/ | while read FILE
 do
-site_name="$(cat lib/gluon/site-select/"$FILE" | grep "site_name" | sed "s/site_name =//; s/,//")"
+site_name="$(cat /lib/gluon/site-select/"$FILE" | grep "site_name" | sed "s/site_name =//; s/,//")"
 #site_code="$(cat "$GLUON_SITEDIR"/site-select/"$FILE" | grep "site_code" | sed "s/site_code =//; s/,//")"
 # We use an external code (the UN/LOCODE (http://www.unece.org/cefact/locode/welcome.html) to select
 # which site.conf should be used where ...
