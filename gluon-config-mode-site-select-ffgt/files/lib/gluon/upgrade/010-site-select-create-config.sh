f662b868593e9492dc6472bad1c5247ad2d22e23
wusel42 <wusel+src@uu.org>
2016-06-04 10:19:22 +0200

    Rework site_select stuff one more time

--- gluon-config-mode-site-select-ffgt/files/lib/gluon/upgrade/010-site-select-create-config.sh
+++ gluon-config-mode-site-select-ffgt/files/lib/gluon/upgrade/010-site-select-create-config.sh
@@ -8,11 +8,11 @@ site_name="$(cat /lib/gluon/site-select/"$FILE" | grep "site_name" | sed "s/site
 #site_code="$(cat "$GLUON_SITEDIR"/site-select/"$FILE" | grep "site_code" | sed "s/site_code =//; s/,//")"
 # We use an external code (the UN/LOCODE (http://www.unece.org/cefact/locode/welcome.html) to select
 # which site.conf should be used where ...
-site_code=" $(basename "$FILE" .conf)"
+site_code="$(basename "$FILE" .conf)"
 
-echo "config site"$site_code"" >> /etc/config/siteselect
+echo "config site '"$site_code"'" >> /etc/config/siteselect
 echo "    option path '/lib/gluon/site-select/"$FILE"'" >> /etc/config/siteselect
-echo "    option sitename"$site_name"" >> /etc/config/siteselect
+echo "    option sitename "$site_name"" >> /etc/config/siteselect
 echo "" >> /etc/config/siteselect
 
 done
