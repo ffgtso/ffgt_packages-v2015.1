68de3101a217a1e44c382e6ea190e6e6f496f0d2
wusel42 <wusel+src@uu.org>
2016-06-07 01:08:19 +0200

    Should now be in a workable state.

--- gluon-luci-site-select/files/usr/lib/lua/luci/model/cbi/admin/site-select.lua
+++ gluon-luci-site-select/files/usr/lib/lua/luci/model/cbi/admin/site-select.lua
@@ -13,7 +13,7 @@ $Id$
 ]]--
 
 local uci = luci.model.uci.cursor()
-
+local cbi = require "luci.cbi"
 local hostname, addr, locode, city
 local s
 local sites = {}
@@ -24,23 +24,43 @@ f.template = "admin/expertmode"
 local s
 local o
 local uplink = "wired"
+local sitename
+local sname
+local siteselect
 
-s = f:section(SimpleSection, nil, [[Hier kann sie site.conf ausgewählt werden ...]])
+s = f:section(SimpleSection, nil, [[
+  Hier kann sie site.conf ausgewählt werden ... <strong>Achtung:</strong> Diese Einstellungen
+  ver&auml;ndern die Kernfunktion des Freifunk-Knotens. Normalerweise wird die passende Region &uuml;ber
+  die Koordinaten automatisch ausgew&auml;hlt. Nur für den Fall, daß dieses fehlschlagen sollte oder spezielle
+  Einstellungen (andere als die normalen Kan&auml;le im Falle von gr&ouml;&szlig;eren Installationen) beni&ouml;tigt
+  werden, diese Daten &auml;ndern! <strong>Auswahl der falschen Region kann zum totalen Ausfall des Knotens
+  f&uuml;hren!</strong>
+]])
+
+local o = s:option(cbi.ListValue, "community", "Region")
+sname = uci:get_first("gluon-node-info", "location")
+siteselect = uci:get("gluon-node-info", sname, "siteselect")
+
+if siteselect then
+  sitename=uci:get("siteselect", siteselect, "sitename") .. ' *'
+  o:value(siteselect, sitename)
+end
 
 uci:foreach('siteselect', 'site',
   function(s)
-    table.insert(sites, s['.name'])
-   end
+    if s['.name'] ~= siteselect then
+      sitename=uci:get("siteselect", s['.name'], "sitename")
+      o:value(s['.name'], sitename)
+    end
+  end
 )
-
-local o = s:option(cbi.ListValue, "community", "Community")
 o.rmempty = false
 o.optional = false
 
 function f.handle(self, state, data)
    if state == FORM_VALID then
     local sname = uci:get_first("gluon-node-info", "location")
-    local siteselect = uci:get_first("gluon-node-info", sname, "siteselect")
+    local siteselect = uci:get("gluon-node-info", sname, "siteselect")
 
     if not siteselect == data.community then
       uci:set("gluon-node-info", sname, "siteselect", data.community)
@@ -48,8 +68,7 @@ function f.handle(self, state, data)
       uci:commit("gluon-node-info")
       -- Copy the proper according to loc site.conf in place.
       os.execute("echo 'Updating system-wide site.conf'")
-      local cmdline=string.format('/sbin/uci get siteselect.%s.path', data.community)
-      local srcfile=string.gsub(sys.exec(cmdline), "\n", "")
+      local srcfile=uci:get("siteselect", data.community, "path")
       os.execute(string.format("echo src=%s com=%s", srcfile, data.community))
       os.execute(string.format("/bin/cp %s /lib/gluon/site.conf", srcfile))
       os.execute('/lib/gluon/site-upgrade &')
