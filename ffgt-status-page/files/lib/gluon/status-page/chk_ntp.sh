b83358ca83720393297124b08bff50a6f96f5fd4
wusel42 <wusel+src@uu.org>
2016-09-17 09:29:46 +0200

    Add warning.

--- ffgt-status-page/files/lib/gluon/status-page/chk_ntp.sh
+++ ffgt-status-page/files/lib/gluon/status-page/chk_ntp.sh
@@ -5,4 +5,4 @@ timeserver="`uci get system.ntp.server | sed -e 's/ / -p /g'`"
 /usr/sbin/ntpd -w -p ${timeserver} 2>/tmp/ntp.chk &
 sleep 5
 ps w | grep -v grep | grep "ntpd -w -p" | awk '/ntp/ {printf("kill %s\n", $1);}' | /bin/sh
-awk </tmp/ntp.chk 'BEGIN{offset="ntp error"} /ntpd: reply from/ {offset=$5; gsub("offset:", "", offset); offset=sprintf("%.1f sec", offset);} END{print offset;}'
+awk </tmp/ntp.chk 'BEGIN{offset="ntp error"} /ntpd: reply from/ {offset=$5; warnstr=""; if (offset >5 || offset <5) warnstr=" (NTP SEEMS TO HAVE ISSUES)"; gsub("offset:", "", offset); offset=sprintf("%.1f sec%s", offset, warnstr);} END{print offset;}'
