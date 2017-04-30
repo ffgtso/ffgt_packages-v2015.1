35f500fb9315032155eb7bb209a658b80498cc68
wusel42 <wusel+src@uu.org>
2017-04-30 12:21:14 +0200

    Fix script

--- gluon-luci-macconfig/files/lib/gluon/upgrade/999-save-wan-mac.sh
+++ gluon-luci-macconfig/files/lib/gluon/upgrade/999-save-wan-mac.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-( WANMAC="`uci get network.wan.macaddr`" ; if [ ! -z "${WANMAC}" ]; then uci get system.@system[0].staticwanmac || uci set system.@syst
-em[0].staticwanmac="${WANMAC}" ; uci commit ; fi ) >/dev/null 2>&1
+( WANMAC="`uci get network.wan.macaddr`" ; \
+ if [ ! -z "${WANMAC}" ]; then \
+  uci get system.@system[0].staticwanmac || uci set system.@system[0].staticwanmac="${WANMAC}" ;\
+  uci commit ;\
+ fi ) >/dev/null 2>&1
